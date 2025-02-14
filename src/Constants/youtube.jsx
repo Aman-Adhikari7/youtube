07deb3a84bdab3470c14c6fbf3be29dd85947963
Aman Adhikari <noattacklost@gmail.com>
2025-02-14 09:01:06 +0530

    "done"

---
+++ src/Constants/youtube.jsx
@@ -0,0 +1,7 @@
+export const BaseUrl = "https://www.googleapis.com/youtube/v3"
+export const API_Key = "AIzaSyDABpO0RO_Zu2_0AYZkq-zEDfjRy2QzkQs"
+export const Vedio_API= "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=In&key="
+
+
+
+
